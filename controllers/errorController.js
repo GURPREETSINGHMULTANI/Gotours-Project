9a7d01ad421d65c57508bcba78745e1a1a011b76
Gurpreet Multani <GURPREETSINGHMULTANI@icloud.com>
2021-08-19 00:49:28 -0400

    Use JWT and Bcrypt to Implement Authentication

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,6 +20,14 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () => {
+    return new AppError('Invalid token, please log in again!', 401);
+}
+
+const handleJWTExpiredError = () => {
+    return new AppError('Your token has expired! Please log in again.', 401);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -63,6 +71,8 @@ module.exports = (err, req, res, next) => {
         if (err.name === 'CastError') error = handleCastErrorDB(error);
         if (err.code === 11000) error = handleDuplicateFieldsDB(error);
         if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+        if (err.name === 'JsonWebTokenError') error = handleJWTError();
+        if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
         sendErrorProd(error, res);
     }
